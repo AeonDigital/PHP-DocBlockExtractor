b3be6745cd430457e69cc100d3093e4ba6055b0e
Rianna Cantarelli <rianna.aeon@gmail.com>
2023-03-22 23:36:42 -0300

    Finalizada verificação de todos tipos de saídas esperados.

---
+++ src/Functions/00_constants_and_variables.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+
+
+
+/**
+ * [1] Constante definida unicamente para permitir testar a obtenção
+ * de suas informações.
+ *
+ * @var string PHPDOCBLOCKEXTRACTOR_NON_USED_CONSTANT_01
+ */
+const PHPDOCBLOCKEXTRACTOR_NON_USED_CONSTANT_01 = "only to perform tests";
+
+
+/**
+ * [2] Constante definida unicamente para permitir testar a obtenção
+ * de suas informações.
+ *
+ * @var string PHPDOCBLOCKEXTRACTOR_NON_USED_CONSTANT_02
+ */
+const PHPDOCBLOCKEXTRACTOR_NON_USED_CONSTANT_02 = "only to perform tests";
+
+
+
+
+
+/**
+ * [3] Variável definida unicamente para permitir testar a obtenção
+ * de suas informações.
+ *
+ * @var int $phpDocBlockExtractor_testVariable01
+ */
+$phpDocBlockExtractor_testVariable01 = 1;
+
+/**
+ * [4] Variável definida unicamente para permitir testar a obtenção
+ * de suas informações.
+ *
+ * @var int $phpDocBlockExtractor_testVariable02
+ */
+$phpDocBlockExtractor_testVariable02 = 2;
